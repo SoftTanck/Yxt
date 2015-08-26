f4c275e0d6a04524b36a816fe96c7d3ef8458fe1
tangce <i0555@cienet.com.cn>
2015-08-26 11:35:17 +0800

    update file.

--- app/src/main/java/com/softtanck/framework/activity/MainActivity.java
+++ app/src/main/java/com/softtanck/framework/activity/MainActivity.java
@@ -4,6 +4,9 @@ package com.softtanck.framework.activity;
 import android.support.v4.app.FragmentTransaction;
 
 import com.softtanck.framework.R;
+import com.softtanck.framework.fragment.AboutMeFragment;
+import com.softtanck.framework.fragment.RankFragment;
+import com.softtanck.framework.fragment.MyTaskFragment;
 import com.softtanck.framework.utils.ScreenUtils;
 
 
@@ -26,10 +29,16 @@ public class MainActivity extends BaseActivity {
     private void initMenu() {
         ScreenUtils.setChenjin(findViewById(R.id.rl), MainActivity.this);
 
-//        fragmentTransaction = getSupportFragmentManager().beginTransaction();
-////        fragmentTransaction.replace(R.id.fl_home_menu, new MenuFragment());
-//        fragmentTransaction.replace(R.id.fl_home_content, new MyTaskFragment());
-//        fragmentTransaction.commit();
+        MyTaskFragment taskFragment = new MyTaskFragment();
+        RankFragment rankFragment = new RankFragment();
+        AboutMeFragment aboutMeFragment = new AboutMeFragment();
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fl_home_content, taskFragment)
+                .add(R.id.fl_home_content, rankFragment)
+                .add(R.id.fl_home_content, aboutMeFragment).
+                hide(rankFragment).
+                hide(aboutMeFragment);
+        fragmentTransaction.commit();
     }
 
 }
---
+++ app/src/main/java/com/softtanck/framework/fragment/RankFragment.java
@@ -0,0 +1,28 @@
+package com.softtanck.framework.fragment;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.softtanck.framework.R;
+
+/**
+ * @author : Tanck
+ * @Description : TODO
+ * @date 8/26/2015
+ */
+public class RankFragment extends BaseFragment {
+    @Override
+    protected int getLayoutView() {
+        return R.layout.fragment_mytask;
+    }
+
+    @Override
+    protected void onAttaching() {
+
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+    }
+}
