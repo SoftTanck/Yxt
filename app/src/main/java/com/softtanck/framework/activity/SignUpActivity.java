b706ac5a5696cddadaf22ed93e70d8fe02adc239
herui <411382221@qq.com>
2015-09-02 15:01:44 +0800

    SignUpActivity

--- app/src/main/java/com/softtanck/framework/activity/SignUpActivity.java
+++ app/src/main/java/com/softtanck/framework/activity/SignUpActivity.java
@@ -1,7 +1,11 @@
 package com.softtanck.framework.activity;
 
 import com.softtanck.framework.R;
+import com.softtanck.framework.utils.ScreenUtils;
 
+/**
+ * 报名详情界面
+ */
 public class SignUpActivity extends BaseActivity {
 
     @Override
@@ -11,6 +15,14 @@ public class SignUpActivity extends BaseActivity {
 
     @Override
     protected void onActivityCreate() {
+        initTitle();
 
+    }
+
+    private void initTitle() {
+        ScreenUtils.setChenjin(findViewById(R.id.sign_up_first_linear), SignUpActivity.this);
+        titleView.setTitle("报名");
+        titleView.setTitleViewBackGround(R.color.white);
+        titleView.setTitleColor(getResources().getColor(R.color.black));
     }
 }
